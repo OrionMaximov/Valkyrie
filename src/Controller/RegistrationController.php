7d16db2f4de218affe8b9df5cd940a59179634d8
OrionMaximov <quentin.nicodeme@gmail.com>
2022-12-12 09:38:22 +0100

    Tjrs pas trouvé mais il existe des assert less than 18

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -50,10 +50,10 @@ class RegistrationController extends AbstractController
 
                     $aujourdhui = date("Y-m-d");
                     $diff = date_diff(date_create($dateNaissance), date_create($aujourdhui));
-                    /* dd($diff); */
+                   /*  dd($diff);   */
                     
 
-                    if ($diff < "18" ) {
+                    if ($diff <= "+18" ) {
                         $user->setRoles(['ROLE_USER']);
                     } else {
                         $user->setRoles(['ROLE_PERVER']);
